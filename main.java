53db7d6ebd915584376067b83f90e8bb10aeb261
Archibald Perez <archieperez99@gmail.com>
2018-11-22 13:53:40 +0100

    main

--- main.java
+++ main.java
@@ -1,1 +1,12 @@
-dqesfhjokmlpùkohugfwqs<fdyuio
+import java.util.*;
+
+/**
+ * 
+ * @author Archibald Perez, Antoine Pontallier, Akhara Sang
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
