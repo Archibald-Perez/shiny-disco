21a19ef4026938f0d00ed23d2e71228606be1f6b
Archibald Perez <archieperez99@gmail.com>
2018-11-22 14:21:36 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ import java.util.*;
 import java.io.*;
 
 /**
- * 
+ * Ouverture de fichier
  * @author Archibald Perez, Antoine Pontallier, Akhara Sang
  *
  */
 public class Main {
-	public static void main(String[] args) {
-		File file = new File("dominos.csv");
-		Scanner scan = new Scanner(file);
+	public static void main(String[] args) throws FileNotFoundException {
+		File dominos = new File("dominos.txt");
+		Scanner scan = new Scanner(dominos);
 		while (scan.hasNextLine()) {
 			System.out.println(scan.nextLine());
 		}
