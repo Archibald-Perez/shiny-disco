dd66a6a4d07e0cc94b63f1e273541c1602d332ca
Akhara <43671530+Student2003@users.noreply.github.com>
2018-11-30 12:13:56 +0100

    Classe encore remasterisé

---
+++ src/Castle.java
@@ -0,0 +1,11 @@
+public class Castle {
+    public int posx;
+    public int posy;
+    public Board board;
+
+    public Castle(int posx,int posy,Board board){
+        this.posx=posx;
+        this.posy=posy;
+        this.board=board;
+    }
+}
---
+++ src/DominoInDeck.java
@@ -0,0 +1,11 @@
+public class DominoInDeck extends Domino {
+
+    public DominoInDeck(int number, char zone1, char zone2, int crown1, int crown2) {
+        this.number = number;
+        this.zone1 = zone1;
+        this.zone2 = zone2;
+        this.crown1 = crown1;
+        this.crown2 = crown2;
+    }
+
+}
