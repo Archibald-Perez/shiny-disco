976eedee4ab6e474c20a52a4f45a143daa9733a4
Akhara <43671530+Student2003@users.noreply.github.com>
2018-11-30 11:38:06 +0100

    Nouvelles classes :D

---
+++ src/Domino.java
@@ -0,0 +1,15 @@
+public class Domino {
+    public int number;
+    public char zone1;
+    public char zone2;
+    public int crown1;
+    public int crown2;
+
+    public Domino(int number, char zone1, char zone2, int crown1, int crown2){
+        this.number = number;
+        this.zone1= zone1;
+        this.zone2 = zone2;
+        this.crown1= crown1;
+        this.crown2 = crown2;
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class Player {
+    public String name;
+    public Boolean isAI;
+    public Color color;
+
+    public Player(String name, Boolean isAI, Color color){
+        this.name = name;
+        this.isAI=isAI;
+        this.color= color;
+    }
+}
+
