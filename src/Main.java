ec0f4ac8e8415e6f38ee72d290d24acc29f8e812
Archibald Perez <archieperez99@gmail.com>
2018-11-30 12:35:55 +0100

    commentaires

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,25 @@ import java.io.*;
  */
 public class Main {
 	public static int nbPlayers;
-	public static void main(String[] args) throws FileNotFoundException {
-		File dominos = new File("dominos.txt");
-		Scanner scan = new Scanner(dominos);
-		while (scan.hasNextLine()) {
-			System.out.println(scan.nextLine());
-		}
+	public static void main(String[] args) {
+		// Lance le menu
+			// Choix du nombre de joueur
+			// Nom des joueurs + AI ou pas
+		// Création des dominos
+			// Lecture des dominos depuis le fichier texte
+			// Créer les objet DominoInDeck
+		// Début de la partie
+		// Choisi l'ordre initial des joueurs
+		// Tant qu'il y a des dominos dans la pioche {
+			// Sélectionne aléatoirement les dominos
+			// Les joueurs choisissent leurs dominos
+			// Boucles des joueurs {
+				// Le joueur place son domino
+			// }
+		// }
+		// Calcule des scores
+		// Affiche le vainqueur
+		// Retourne au menu
 	}
 	public static Byte chooseNbPlayers(){ //la fonction qui récupère le nombre de joueur
 		Scanner a = new Scanner(System.in);
@@ -39,5 +52,12 @@ public class Main {
 		System.out.println("Domi'Nations");
 		System.out.println("1. Jouer une partie");
 		System.out.println("2. Quitter");
+	}
+	public static void createDominos() throws FileNotFoundException {
+		File dominos = new File("dominos.txt");
+		Scanner scan = new Scanner(dominos);
+		while (scan.hasNextLine()) {
+			System.out.println(scan.nextLine());
+		}
 	}
 }
