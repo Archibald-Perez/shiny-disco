a97caf5cf8b280c3b1be3cf1a313f21a6345f6c3
Archibald Perez <archieperez99@gmail.com>
2018-12-07 10:42:42 +0100

    comment

--- src/DominoOnBoard.java
+++ src/DominoOnBoard.java
@@ -2,6 +2,10 @@ public class DominoOnBoard extends Domino{
     public int posx;
     public int posy;
     public int orientation;
+    	// 0 siginfie que la zone 2 est en postion x+1, y
+    	// 1 siginfie que la zone 2 est en postion x, y-1
+    	// 2 siginfie que la zone 2 est en postion x-1, y
+		// 3 siginfie que la zone 2 est en postion x, y+1
     public Board board;
 
         public DominoOnBoard(int number, String zone1, String zone2, int crown1, int crown2, Board board, int posx, int posy, int orientation){
