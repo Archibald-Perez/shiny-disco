d15e3adbddef6c973373b66155a1d01a9a615e95
Akhara <43671530+Student2003@users.noreply.github.com>
2018-12-07 10:47:01 +0100

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,17 +1,32 @@
 import java.awt.*;
+import java.util.*;
+import java.util.List;
+import java.util.Random;
 
 public class Player {
     public String name;
     public Boolean isAI;
     public Color color;
+    public Byte ordre;
+    public static List<Integer> sequence = new ArrayList<Integer>();
 
-    public Player(String name, Boolean isAI, Color color){
+    public Player(String name, Boolean isAI, Color color, Byte ordre){
         this.name = name;
         this.isAI=isAI;
         this.color= color;
+        this.ordre = ordre;
     }
 
-    public static void sequence(){
+    public static void sequenceInitial(){
+        Random rand = new Random();
+        Byte nbPlayer = PlayerManager.nbPlayer;
+        for (int i = 1; i<nbPlayer+1; i++){
+            sequence.add(i);
+
+        }
+        for (Byte i = 0; i<nbPlayer; i++){
+            int sequence = rand.nextInt(nbPlayer)+1;
+        }
 
     }
     @Override
