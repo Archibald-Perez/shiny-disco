7f48db56536df121a2494416bd7486f75e7906dc
Archibald Perez <archieperez99@gmail.com>
2018-12-07 08:37:12 +0100

    Update DominoManager.java

--- src/DominoManager.java
+++ src/DominoManager.java
@@ -2,15 +2,15 @@ import java.io.*;
 import java.util.*;
 
 public class DominoManager {
-	public static List<Domino> Dominos = new ArrayList<>();
+	public static List<Domino> dominos = new ArrayList<>();
 	
 	public static void createDominos() throws FileNotFoundException {
-		File dominos = new File("dominos.txt");
-		Scanner scan = new Scanner(dominos);
+		File dominoFile = new File("dominos.txt");
+		Scanner scan = new Scanner(dominoFile);
 		while (scan.hasNextLine()) {
 			String[] elements = scan.nextLine().split(",");
 			Domino domino = new Domino(Integer.parseInt(elements[4]),elements[1],elements[3],Integer.parseInt(elements[0]),Integer.parseInt(elements[2]));
-			Dominos.add(domino);
+			dominos.add(domino);
 		}
 	}
 }
